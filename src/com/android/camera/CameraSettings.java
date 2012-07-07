cd29228eb1d37fe25924a4ba5461ac3fb28d53e3
LorD ClockaN <davor@losinj.com>
2012-07-07 17:43:42 +0200

    camera: support for ZSL on samsung cameras
     * Various changes from CAF to support ZSL.
    
    Change-Id: I52d2f500470d5fd6127b8f170bc8bfd4aa9cdb57
    
    camera: Handle error callback
    
    Add function to handle error callback from HAL
    The error callback is triggered by daemon crash,
    which will send error msg to HAL and call back to app
    Application will stop camera/camcorder gracefully
    
    Change-Id: If0f501d88ac06b06c6d051cffcb91800c3feda38
    
    Camera : Restart preview when the snapshot aspect ratio changes
    
    Restarting of preview is required when user changes to a different
    picture resolution whose aspect ratio is different, as the buffers
    need to be released and requested again with new preview resolution.
    
    Change-Id: If7095f3e9879e8dcbafe57776d8dda67f9b79b74
    
    Camera: Fix issue with preview callback buffer size mismatch.
    
    If android.hardware.Camera.PreviewCallback  is implemented in
    the application layer, then the callback buffers will be delivered
    to the application. A crash was seen in switching between picture
    sizes with different aspect ratios.When aspect ratio changed the
    preview was restarted after some delay. This was causing intermediate
    buffers size to not match with the newly selected size.
    Fixed this by calling stopPreview immediately after the aspect
    ratio changed. Start preview will be called at a later point.
    
    CRs-Fixed: 327767
    
    Change-Id: I5f5a5f2a29e90e5484f98204a49400b1d96a800e
    (cherry picked from commit ef175f83d4b225a0af8544c83dc727baa67d5940)
    
    Conflicts:
    
    	res/values/config.xml
    	src/com/android/camera/Camera.java
    	src/com/android/camera/CameraSettings.java

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -56,6 +56,7 @@ public class CameraSettings {
     
     public static final String KEY_POWER_SHUTTER = "pref_power_shutter";
     public static final String KEY_VOLUME_ZOOM = VolumeZoomPreference.KEY;
+    public static final String KEY_ZSL = "pref_camera_zsl_key";
 
     public static final String EXPOSURE_DEFAULT_VALUE = "0";
 
