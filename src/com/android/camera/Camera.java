3f0914735972b8e73f6f41a9237566e5616a37f4
LorD ClockaN <davor@losinj.com>
2012-07-07 22:36:00 +0200

    Added option to select location of saved videos / photos
    Change-Id: I9843e94fe30d20911b5b294ecf128c80503c1147
    
    Conflicts:
    
    	src/com/android/camera/CameraSettings.java

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -195,6 +195,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
 
     private CameraSound mCameraSound;
 
+    private String mStorage;
+
     private Runnable mDoSnapRunnable = new Runnable() {
         public void run() {
             onShutterButtonClick();
@@ -513,7 +515,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         MessageQueue queue = Looper.myQueue();
         queue.addIdleHandler(new MessageQueue.IdleHandler() {
             public boolean queueIdle() {
-                Storage.ensureOSXCompatible();
+                Storage.ensureOSXCompatible(mStorage);
                 return false;
             }
         });
@@ -529,7 +531,8 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         // Update last image if URI is invalid and the storage is ready.
         if ((mThumbnail == null || !Util.isUriValid(mThumbnail.getUri(), mContentResolver))
                 && mPicturesRemaining >= 0) {
-            mThumbnail = Thumbnail.getLastThumbnail(mContentResolver);
+           mThumbnail = Thumbnail.getLastThumbnail(mContentResolver,
+                Storage.generateBucketId(mStorage));
         }
         if (mThumbnail != null) {
             mThumbnailView.setBitmap(mThumbnail.getBitmap());
@@ -1127,7 +1130,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
                 int height, long dateTaken, int previewWidth) {
             String title = Util.createJpegName(dateTaken);
             int orientation = Exif.getOrientation(data);
-            Uri uri = Storage.addImage(mContentResolver, title, dateTaken,
+            Uri uri = Storage.addImage(mContentResolver, mStorage, title, dateTaken,
                     loc, orientation, data, width, height);
             if (uri != null) {
                 boolean needThumbnail;
@@ -1256,6 +1259,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         getPreferredCameraId();
+        mStorage = CameraSettings.readStorage(mPreferences);
         powerShutter(mPreferences);
         String[] defaultFocusModes = getResources().getStringArray(
                 R.array.pref_camera_focusmode_default_array);
@@ -1397,6 +1401,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         final String[] OTHER_SETTING_KEYS = {
                 CameraSettings.KEY_RECORD_LOCATION,
                 CameraSettings.KEY_POWER_SHUTTER,
+                CameraSettings.KEY_STORAGE,
                 CameraSettings.KEY_VOLUME_ZOOM,
                 CameraSettings.KEY_PICTURE_SIZE,
                 CameraSettings.KEY_FOCUS_MODE,
@@ -1478,7 +1483,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     }
 
     private void checkStorage() {
-        mPicturesRemaining = Storage.getAvailableSpace();
+        mPicturesRemaining = Storage.getAvailableSpace(mStorage);
         if (mPicturesRemaining > Storage.LOW_STORAGE_THRESHOLD) {
             mPicturesRemaining = (mPicturesRemaining - Storage.LOW_STORAGE_THRESHOLD)
                     / Storage.PICTURE_SIZE;
@@ -2393,7 +2398,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     }
 
     private void gotoGallery() {
-        MenuHelper.gotoCameraImageGallery(this);
+        MenuHelper.gotoCameraImageGallery(this, Storage.generateBucketId(mStorage));
     }
 
     private boolean isCameraIdle() {
@@ -2511,6 +2516,12 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         boolean recordLocation = RecordLocationPreference.get(
                 mPreferences, getContentResolver());
         mLocationManager.recordLocation(recordLocation);
+
+        String storage = CameraSettings.readStorage(mPreferences);
+        if (!storage.equals(mStorage)) {
+            mStorage = storage;
+            checkStorage();
+        }
         
         if (mParameters.isZoomSupported())
             mVolumeZoom = VolumeZoomPreference.get(mPreferences, mContentResolver);
